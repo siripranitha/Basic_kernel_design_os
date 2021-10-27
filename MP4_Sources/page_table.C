a4ea11fc9bc0a47d656d5dc0d282d0b2494d8a76
siri <siri@tamu.edu>
2021-10-26 20:25:23 -0500

    skeleton of the code

--- MP4_Sources/page_table.C
+++ MP4_Sources/page_table.C
@@ -14,6 +14,7 @@ unsigned long PageTable::shared_size = 0;
 #define WRITE_BIT    2  // bit 1 ,the r/w bit to be set to 1
 #define USER_LEVEL_BIT  4
 // referenced http://www.osdever.net/tutorials/view/implementing-basic-paging
+// implementing a linked list in c
 
 
 void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
@@ -24,6 +25,7 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
   PageTable::shared_size = _shared_size;
   PageTable::kernel_mem_pool = _kernel_mem_pool;
   PageTable::process_mem_pool = _process_mem_pool;
+  vmpool_count = 0;
 
    Console::puts("Initialized Paging System\n");
 
@@ -34,15 +36,18 @@ PageTable::PageTable()
   unsigned long address=0; 
   unsigned int i;
   
-  page_directory = ( unsigned long*)(kernel_mem_pool->get_frames(1)*PAGE_SIZE);
-  unsigned long *page_table = ( unsigned long*)(kernel_mem_pool->get_frames(1)*PAGE_SIZE);
+  page_directory = ( unsigned long*)(process_mem_pool->get_frames(1)*PAGE_SIZE);
+  unsigned long *page_table = ( unsigned long*)(process_mem_pool->get_frames(1)*PAGE_SIZE);
    
     
   for(i=0; i<1024; i++){
-    page_table[i] = address | WRITE_BIT | PRESENT_BIT; 
+    page_table[i] = address | WRITE_BIT | PRESENT_BIT | USER_LEVEL_BIT; 
     // attribute set to: supervisor level, read/write, present(011 in binary)
     address = address + 4096; // 4096 = 4kb
   };
+  page_table[1023] = (unsigned long) page_table;
+  page_table[1023] = page_table[1023] | PRESENT_BIT;
+  // attribute should be supervisor level , read only , present - 001
 
   page_directory[0] = (unsigned long) page_table; 
   // entry of page directory set to page table
@@ -51,12 +56,17 @@ PageTable::PageTable()
 
 
   for(i=1; i<1024; i++){
-  page_directory[i] = 0 | WRITE_BIT; 
+  page_directory[i] = 0 | WRITE_BIT |USER_LEVEL_BIT; 
   // attribute set to: supervisor level, read/write, not present(010 in binary)
   };
+
+  page_directory[1023] = (unsigned long)page_directory;
+  page_directory[1023] = page_directory[1023] | PRESENT_BIT;
+
+  // writing it for recursive loop  
     Console::puts("Constructed Page Table object\n");
 }
-
+    
 
 void PageTable::load()
 {
@@ -77,43 +87,50 @@ void PageTable::enable_paging()
 
 void PageTable::handle_fault(REGS * _r)
 {
-    unsigned long error_code = _r->err_code;
+  unsigned long error_code = _r->err_code;
   unsigned long page_fault_address = read_cr2();
-  unsigned long *page_directory_current = (unsigned long *)read_cr3();
-
-  unsigned long fault_addr_page_dir_entry = page_fault_address>>22;
-  unsigned long fault_addr_page_table_entry = page_fault_address>>12;
-  unsigned long *new_page=NULL;
-  unsigned long *page_table_containing_the_page = NULL;
   
+  unsigned long *page_directory_current = PageTable::PDE_address(page_fault_address);
+  unsigned long *page_table_containing_the_page = PageTable::PTE_address(page_fault_address);
 
+  unsigned long page_table_entry_number = (_page_no>>12)& 0x000003FF;
+  unsigned long page_directory_entry_number = (_page_no>>22)& 0x000003FF;
 
+    
+// is the address registered in vmpools? check..
+  for (int i=0;i<=vmpool_count;i++){
+    assert(vmpool_list[i]->is_legitimate(page_fault_address));
+  }
 
   if (error_code & PRESENT_BIT == 0){
     // that means the page is not presennt. now to check whether the problem lies at page table level or page dir level
     // first check is to see whether the page directory entry corresponding to page address is valid.
-    if (page_directory_current[fault_addr_page_dir_entry]&PRESENT_BIT==1){
+    if (page_directory_current[page_directory_entry_number]&PRESENT_BIT==1){
       // page directory entry is present, that is page table is initialised, but page table entry is missing.
-      
-      
+      page_table_containing_the_page[page_table_entry_number] = (PageTable::process_mem_pool->get_frames(1)*PAGE_SIZE)| WRITE_BIT | PRESENT_BIT|USER_LEVEL_BIT;
+    } 
+    
+    }
 
-      page_table_containing_the_page = (unsigned long *)(page_directory_current[fault_addr_page_dir_entry] & 0xFFFFF000);
-      page_table_containing_the_page[page_table_containing_the_page[fault_addr_page_table_entry] & 0x3FF] = (PageTable::process_mem_pool->get_frames(1)*PAGE_SIZE)| WRITE_BIT | PRESENT_BIT;
-    } }else{
+    else{
       // we have to create a page dir entry and corresponding page table entry for the faulty address
-        page_directory_current[fault_addr_page_dir_entry] = (unsigned long)(kernel_mem_pool->get_frames(1)*PAGE_SIZE)| WRITE_BIT | PRESENT_BIT;
-        page_table_containing_the_page = (unsigned long *)(page_directory_current[fault_addr_page_dir_entry] & 0xFFFFF000);
-        
+        page_directory_current[page_directory_entry_number] = (unsigned long)(process_mem_pool->get_frames(1)*PAGE_SIZE)| WRITE_BIT | PRESENT_BIT|USER_LEVEL_BIT;
+        //page_table_containing_the_page = (unsigned long *)(page_directory_current[fault_addr_page_dir_entry] & 0xFFFFF000);
+        //IS THIS REQUIRED ? I DONT THINK SO??
+
         for(int i=0; i<1024; i++){
-            page_table_containing_the_page[i] =  USER_LEVEL_BIT ; 
+            page_table_containing_the_page[i] =  USER_LEVEL_BIT ;  // write bit not used bcoz directory?
               // attribute set to: user level
               };
+        page_table_containing_the_page[1023] = (unsigned long) page_table_containing_the_page;
+        page_table_containing_the_page[1023] = page_table_containing_the_page[1023] | PRESENT_BIT; //put it to supervisor level?
 
-      page_table_containing_the_page[fault_addr_page_table_entry&0x3FF] = (PageTable::process_mem_pool->get_frames(1)*PAGE_SIZE)| WRITE_BIT | PRESENT_BIT;
+      page_table_containing_the_page[page_table_entry_number] = (PageTable::process_mem_pool->get_frames(1)*PAGE_SIZE)| WRITE_BIT | PRESENT_BIT;
 
     }
 
-  
+    return;
+
 
 
     Console::puts("handled page fault\n");
@@ -121,11 +138,33 @@ void PageTable::handle_fault(REGS * _r)
 
 void PageTable::register_pool(VMPool * _vm_pool)
 {
-    assert(false);
+    vmpool_list[vmpool_count] = _vm_pool;
+    vmpool_count = vmpool_count+1;
+
     Console::puts("registered VM pool\n");
 }
 
+unsigned long* PageTable::PDE_address(unsigned long addr)
+{
+    unsigned long pde_entry = addr>>22;
+    return 0xFFFFF000 |(pde_entry);
+}
+
+unsigned long* PageTable::PTE_address(unsigned long addr)
+{
+    unsigned long pte_entry = addr>>12;
+    return 0x03FF0000|pte_entry;
+    
+}
+
 void PageTable::free_page(unsigned long _page_no) {
-    assert(false);
-    Console::puts("freed page\n");
+    unsigned long * pte = PageTable::PTE_address(_page_no);
+    unsigned long page_table_entry_number = (_page_no>>12)& 0x000003FF;
+    // page table entry = page table number, left shift by 12 bits and make & operator with 0x000003FF;
+    pte[page_table_entry_number] = 0|WRITE_BIT|USER_LEVEL_BIT //110(user level,can write, not present)
+    // release frames in frame pool?
+
+    //PageTable::load();
+    // flush tlb here or in vm pool?
+
 }
--- MP4_Sources/vm_pool.C
+++ MP4_Sources/vm_pool.C
@@ -44,26 +44,88 @@
 /* METHODS FOR CLASS   V M P o o l */
 /*--------------------------------------------------------------------------*/
 
+#define MAX_COUNT = 200 //PRONE TO CHANGE, MAY BE DERIVE FROM PAGE SIZE ITSELF
+#define PAGE_SIZE = Machine::PAGE_SIZE;
+
+
+
 VMPool::VMPool(unsigned long  _base_address,
                unsigned long  _size,
                ContFramePool *_frame_pool,
                PageTable     *_page_table) {
-    assert(false);
+    base_address = _base_address;
+    size = _size;
+    frame_pool = _frame_pool;
+    page_table = _page_table;
+    page_table->register_pool(this);
+
+    allocated_region = struct allocated_region* (base_address); 
+    // pointer starts from base addr, that means in first page i guess.
+    region_iterator = 0;
+
     Console::puts("Constructed VMPool object.\n");
 }
 
 unsigned long VMPool::allocate(unsigned long _size) {
-    assert(false);
+  // used lk -> address->, base,base+_size
+  //page table ->
+
+    unsigned int page_count = _size/PAGE_SIZE;
+    unsigned long rem = _size%PAGE_SIZE;
+
+    if (rem>0){
+      page_count = page_count+1; 
+    }
+    unsigned long final_size = page_count*PAGE_SIZE;
+    
+    assert((region_iterator+page_count)<=MAX_COUNT-1);
+
+    if (region_iterator==0){
+      allocated_region[region_iterator].start_address_region = base_address+PAGE_SIZE;
+      }else{
+      allocated_region[region_iterator].start_address_region = allocated_region[region_iterator-1].start_address_region+allocated_region[region_iterator-1].size_of_region;
+      }
+    
+    allocated_region[region_iterator].size_of_region = final_size;
+    region_iterator = region_iterator+1;
+    return allocated_region[region_iterator].start_address_region+final_size;
+    
     Console::puts("Allocated region of memory.\n");
 }
 
 void VMPool::release(unsigned long _start_address) {
-    assert(false);
+ // mark those invalid
+  unsigned int index ;
+    for (int i=0;i<MAX_COUNT;i++){
+      if (allocated_region[i].start_address_region == _start_address){
+        index = i;
+        break;
+      }
+    }
+
+  unsigned int page_count = allocated_region[index].size_of_region/PAGE_SIZE;
+  unsigned long addr ;
+    for (int i = 0;i<page_count){
+      addr = allocated_region[index]+i*PAGE_SIZE;
+      page_table.free_page(addr);
+    }
+
+  for (int i =index;i<region_iterator-1;i++){
+    allocated_region[i] = allocated_region[i+1];
+  }
+  region_iterator = region_iterator -1;
+  page_table.load();
+
     Console::puts("Released region of memory.\n");
 }
 
 bool VMPool::is_legitimate(unsigned long _address) {
-    assert(false);
+  // is it bw/w base and base+size or should it be present in allocated list as well?
+    if ((_address>=base_address)&&((base_address+size)>_address)){
+      return true;
+    }else{
+      return false;
+    }
     Console::puts("Checked whether address is part of an allocated region.\n");
 }
 
